eea2abcf8024140cc7433c01997aa39e491609dd
Shubham Parmar <imshubhamparmar@gmail.com>
2021-10-19 19:05:59 +0530

    insertion sort

---
+++ sorting/InsertionSort.js
@@ -0,0 +1,19 @@
+const insertionSort = (arr) => {
+  const list = [...arr];
+
+  for (let i = 1; i < arr.length; i++) {
+    let value = list[i];
+    let hole = i;
+
+    while (hole > 0 && list[hole - 1] > value) {
+      list[hole] = list[hole - 1];
+      hole--;
+    }
+    list[hole] = value;
+  }
+
+  return list;
+};
+
+const arr = [2, 7, 4, 1, 5, 3];
+console.log(insertionSort(arr));
